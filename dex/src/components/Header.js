db7000ea1079b9c4b2aee1921ee3aa91c54981a0
Subash Shumsher Gautam <51912776+shumshersubashgautam@users.noreply.github.com>
2023-02-11 18:01:47 +0545

    Update Header.js

--- dex/src/components/Header.js
+++ dex/src/components/Header.js
@@ -1,8 +1,40 @@
 import React from 'react'
+import Logo from "../11.jpg";
+import Eth from "../eth.svg";
+import { Link } from "react-router-dom";
 
-function Header() {
+
+
+function Header(props) {
+  const {address, isConnected, connect} = props;
+
   return (
-    <div>Header</div>
+    <header>
+    <div className="leftH">
+    
+      
+        <Link to="/" className="link">
+          <div className="headerItem">Swap</div>
+        </Link>
+        <Link to="/tokens" className="link">
+          <div className="headerItem">Tokens</div>
+        </Link>
+      </div>
+      <div className="middleH">
+      <img src={Logo} alt="logo" className="logo" />
+      <h2>SHUMSHER-DEX-AGGREGATOR </h2> 
+      </div>
+        <div className="rightH">
+        <div className="headerItem">
+       
+          <img src={Eth} alt="eth" className="eth" />
+          Ethereum
+        </div>
+        <div className="connectButton" onClick={connect}>
+        {isConnected ? (address.slice(0,4) +"..." +address.slice(38)) : "Connect"}
+        </div>
+      </div>
+    </header>
   )
 }
 
